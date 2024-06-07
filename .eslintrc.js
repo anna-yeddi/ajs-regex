59794cfce671e17e93802f7b11f241db64cd5625
Anna Kova <m3ta007@gmail.com>
2024-06-06 20:20:03 -0400

    Added Jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,10 @@ module.exports = {
   'extends': 'eslint:recommended',
   'overrides': [
     {
+      'files': ['**/*/*.test.js'],
+      'plugins': ['jest'],
+      'extends': ['plugin:jest/recommended'],
+      'rules': { 'jest/prefer-expect-assertions': 'off' }
     }
   ],
   'parserOptions': {
