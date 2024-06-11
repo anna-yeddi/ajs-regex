2813d6bc08fa4a82ecb2e0c7be115f1bda2b9395
Anna Kova <m3ta007@gmail.com>
2024-06-10 22:04:43 -0400

    Added a function to sort characters by their health

--- src/js/sort.js
+++ src/js/sort.js
@@ -2,7 +2,7 @@ export default function sortCharactersByHealth(characters) {
   function compareHealth(a, b) {
     return b.health - a.health;
   }
-  const sorted = characters.sort(compareHealth)
+  const sorted = characters.sort(compareHealth);
   
   return sorted;
 }
