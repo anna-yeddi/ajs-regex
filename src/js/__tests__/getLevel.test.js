3d552364b2cea20e7301d7fdccc86ed5cf869b2c
Anna Kova <m3ta007@gmail.com>
2024-06-13 19:58:31 -0400

    Adding test coverage for getLevel

--- src/js/__tests__/getLevel.test.js
+++ src/js/__tests__/getLevel.test.js
@@ -3,22 +3,19 @@ import fetchData from "../http";
 
 jest.mock("../http");
 
-beforeEach(() => {
-  jest.resetAllMocks();
+describe("getLevel", () => {
+  it("should return the level when response status is ok", () => {
+    const mockLevel = 10;
+    fetchData.mockReturnValue({ status: "ok", level: mockLevel });
+
+    const response = getLevel(1);
+    expect(response).toEqual(`Your current level is: ${mockLevel}`);
+  });
+
+  it("should return an error message when response status is not ok", () => {
+    fetchData.mockReturnValue({ status: "error" });
+
+    const response = getLevel(1);
+    expect(response).toBe(`The level information is currently unavailable`);
+  });
 });
-
-test("should call fetchData once while calling getLevel", () => {
-  // fetchData.mockReturnValue(JSON.stringify({}));
-  const mockFetch = jest.fn().mockResolvedValue((this.status = "ok"));
-
-  const response = getLevel(1);
-  expect(response).toEqual(`The level information is currently unavailable`);
-  expect(mockFetch).toHaveBeenCalledWith("https://server/user/1");
-});
-
-// test("should call fetchData once while calling getLevel", () => {
-//   fetchData.mockReturnValue(JSON.stringify({}));
-
-//   const response = getLevel([1].status);
-//   expect(response).toEqual(`Your current level is: `);
-// });
